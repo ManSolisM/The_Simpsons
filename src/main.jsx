8725b9219fe381767c395270f0d39f646c9378b5
Manuel <trabajscolar@gmail.com>
2026-03-16 13:57:32 -0600

    The Simpsons

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import '@/styles/globals.css'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
